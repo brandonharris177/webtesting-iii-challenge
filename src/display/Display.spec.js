1c753cf09f48dff2c044faf0b2aa7c89df0bba88
Brandon-Harris <brandonharris177@gmail.com>
2019-09-16 16:38:13 -0600

    led tests

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -13,27 +13,32 @@ test('should match snapshot', () => {
 
 test('is open/unlocked', () => {
     const {getByText} = render(<Display closed = {false} locked = {false}/>);
-    getByText(/open/i)
-    getByText(/unlocked/i)
-    
+    const open = getByText(/open/i)
+    const unlocked = getByText(/unlocked/i)
+    expect (open.className).toMatch(/led green-led/i)
+    expect (unlocked.className).toMatch(/led green-led/i)
 });
 
 test('is closed/unlocked', () => {
     const {getByText} = render(<Display closed = {true} locked = {false}/>);
-    getByText(/closed/i)
-    getByText(/unlocked/i)
-    
+    const closed = getByText(/closed/i)
+    const unlocked = getByText(/unlocked/i)  
+    expect (closed.className).toMatch(/led red-led/i)
+    expect (unlocked.className).toMatch(/led green-led/i)
 });
 
 test('is open/locked', () => {
     const {getByText} = render(<Display closed = {false} locked = {true}/>);
-    getByText(/open/i)
-    getByText(/locked/i)
-    
+    const open = getByText(/open/i)
+    const locked = getByText(/locked/i)
+    expect (open.className).toMatch(/led green-led/i)
+    expect (locked.className).toMatch(/led red-led/i)
 });
 
 test('is closed/locked', () => {
     const {getByText} = render(<Display closed = {true} locked = {true}/>);
-    getByText(/closed/i)
-    getByText(/locked/i) 
+    const closed = getByText(/closed/i)
+    const locked = getByText(/locked/i) 
+    expect (closed.className).toMatch(/led red-led/i)
+    expect (locked.className).toMatch(/led red-led/i)
 });
