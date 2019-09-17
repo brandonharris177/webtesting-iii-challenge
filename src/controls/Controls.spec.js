1f2056171bd4b2191ec5a651fb01601d5b3b1a58
Brandon-Harris <brandonharris177@gmail.com>
2019-09-16 19:06:34 -0600

    controls tests implemented

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,41 +1,44 @@
 // Test away!
-import { render } from '@testing-library/react';
 import React from 'react';
-import '@testing-library/react/cleanup-after-each'; // function at the top
-import "jest-dom/extend-expect"; // added functionality (disabled)
-import Control from './Controls'
+import Controls from './Controls';
+import {render} from '@testing-library/react';
 
-describe ("test buttons", ()=>{
-    describe ("test if buttons exist",()=>{
-        it("find the buttons at initial state", ()=>{
-            //render component
-            const control = render(<Control closed={false} locked={false}/>) //diff combinations.
-            //grab the element
-            control.getByText('Lock Gate') // dot notation to find // not using var just checking
-            control.getByText('Close Gate') // dot notation to find
-            //
-            expect(control.getByText('Lock Gate')).toBeDisabled()
-            expect(control.getByText('Close Gate')).not.toBeDisabled()
-        })
-        it("find the buttons open gate", ()=>{
-            //render component
-            const control = render(<Control closed={true} locked={false}/>) //diff combinations.
-            //grab the element
-            control.getByText('Lock Gate') // dot notation to find // not using var just checking
-            control.getByText('Open Gate') // dot notation to find
-            //
-            expect(control.getByText('Lock Gate')).not.toBeDisabled()
-            expect(control.getByText('Open Gate')).not.toBeDisabled()
-        })
-        it("unlock Gate", ()=>{
-            //render component
-            const control = render(<Control closed={true} locked={true}/>) //diff combinations.
-            //grab the element
-            control.getByText('Unlock Gate') // dot notation to find // not using var just checking
-            control.getByText('Open Gate') // dot notation to find
-            //
-            expect(control.getByText('Unlock Gate')).not.toBeDisabled()
-            expect(control.getByText('Open Gate')).toBeDisabled()
-        })
-    })
-})
+test('Controls renders without crashing', () => {
+    render(<Controls />);
+});
+
+test('Controls should match snapshot', () => {
+    expect(render(<Controls/>)).toMatchSnapshot();
+});
+
+test('is open/unlocked', () => {
+    const {getByText} = render(<Controls closed = {true} locked = {true}/>);
+    const openGateButton = getByText(/open gate/i)
+    const unlockGateButton = getByText(/unlock gate/i)
+    expect (openGateButton.disabled).toBeTruthy()
+    expect (unlockGateButton.className).toBeTruthy()
+});
+
+test('is closed/unlocked', () => {
+    const {getByText} = render(<Controls closed = {false} locked = {true}/>);
+    const closeGateButton = getByText(/close gate/i)
+    const unlockGateButton = getByText(/unlock gate/i)  
+    expect (closeGateButton.disabled).toBeTruthy()
+    expect (unlockGateButton.className).toBeTruthy()
+});
+
+test('is open/locked', () => {
+    const {getByText} = render(<Controls closed = {true} locked = {false}/>);
+    const openGateButton = getByText(/open gate/i)
+    const lockGateButton = getByText(/lock gate/i)
+    expect (openGateButton.disabled).toBeFalsy()
+    expect (lockGateButton.className).toBeTruthy()
+});
+
+test('is closed/locked', () => {
+    const {getByText} = render(<Controls closed = {false} locked = {false}/>);
+    const closeGateButton = getByText(/close Gate/i)
+    const lockGateButton = getByText(/lock Gate/i) 
+    expect (closeGateButton.disabled).toBeFalsy()
+    expect (lockGateButton.className).toBeTruthy()
+});
